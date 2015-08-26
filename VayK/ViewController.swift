9fb521425d68ccf961ab401e07b3c88af965b437
Matthew Goldspink <matt@vlocity.com>
2015-08-26 13:21:43 +0200

    Initial Commit

---
+++ VayK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VayK
+//
+//  Created by Matthew Goldspink on 8/26/15.
+//  Copyright (c) 2015 mgoldspink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ VayKTests/VayKTests.swift
@@ -0,0 +1,36 @@
+//
+//  VayKTests.swift
+//  VayKTests
+//
+//  Created by Matthew Goldspink on 8/26/15.
+//  Copyright (c) 2015 mgoldspink. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class VayKTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
